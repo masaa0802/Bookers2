d33cb6a6b4c36a1a2ef92cc68c6ac8461c1a8c15
masaa0802 <masaakigoto0802@gmail.com>
2021-09-18 02:20:53 +0000

    Second Commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key ='fb93a7a64df4d24b4b20f61068cd7e9da2c6a8df3c9826107d5f64c73b3369fb152e310afe720ac18890508f41179655e51e0e1ec37d17877b2fa226b09b1cf8'
