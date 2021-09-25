78d2046c726b3d69652e0f22fc31d274a3f58bba
masaa0802 <masaakigoto0802@gmail.com>
2021-09-25 15:37:06 +0000

    Second Commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,12 @@
 class Book < ApplicationRecord
   belongs_to :user
-  attachment :profile_image
 
   validates :title, presence: true
   validates :body, presence: true, length: {maximum: 200}
 
+  has_many :favorites, dependent: :destroy
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+  has_many :book_comments, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "home/about" =>"homes#about",as:"about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books
   resources :users
+  resources :books do
+    resource :favorite, only: [:create, :destroy]
+    resources :book_comments,  only: [:create, :destroy]
+  end
+
 end
