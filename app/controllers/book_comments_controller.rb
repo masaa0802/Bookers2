dcf6de286fdf25759b760c4e46052a662a801c88
masaa0802 <masaakigoto0802@gmail.com>
2021-10-04 03:02:54 +0000

    非同期通信修正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,19 +2,19 @@ class BookCommentsController < ApplicationController
 
   def create
 		@book = Book.find(params[:book_id])
-		book_comment = current_user.book_comments.new(comment_params)
-		book_comment.book_id = @book.id
-		book_comment.save!
+		@book_comment = current_user.book_comments.new(comment_params)
+		@book_comment.book_id = @book.id
+		@book_comment.save!
   end
 
   def destroy
 		@book = Book.find(params[:book_id])
-  	book_comment =  BookComment.find(params[:id])
+  	book_comment = @book.book_comments.find(params[:id])
 		book_comment.destroy
   end
 
   private
   def comment_params
-    params.require(:book_comment). permit(:user_id,:book_id,:comment)
+    params.require(:book_comment). permit(:comment)
   end
 end
