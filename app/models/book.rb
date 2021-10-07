dd03b823d9a37ae472df6044299626937c7323a4
masaa0802 <masaakigoto0802@gmail.com>
2021-10-07 04:59:26 +0000

    9a追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
+  is_impressionable counter_cache: true
 
   validates :title, presence: true
   validates :body, presence: true, length: {maximum: 200}
